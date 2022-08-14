ed186f44bfb553392fa9050e227e34bbc5bb0973
Almu aja aha Ali <Kripper2004@mail.ru>
2022-08-15 02:16:30 +0300

    Подготовка классов адаптера для главного экрана

--- app/src/main/java/com/example/kode/test_task/ui/activities/single_activity_fragments/main/models/WorkerInfoUIModel.kt
+++ app/src/main/java/com/example/kode/test_task/ui/activities/single_activity_fragments/main/models/WorkerInfoUIModel.kt
@@ -1,9 +1,14 @@
 package com.example.kode.test_task.ui.activities.single_activity_fragments.main.models
 
+import com.example.kode.test_task.ui.core.recycler_view.BaseRecyclerViewModel
+
 data class WorkerInfoUIModel(
     val avatarUrl: String,
     val name: String,
     val lastName: String,
     val userTag: String,
     val position: String
-)
+) : BaseRecyclerViewModel<Int> {
+
+    override fun getRVEqualsValue(): Int = hashCode()
+}
---
+++ app/src/main/java/com/example/kode/test_task/ui/activities/single_activity_fragments/main/recycler_view/MainViewHolderFactory.kt
@@ -0,0 +1,4 @@
+package com.example.kode.test_task.ui.activities.single_activity_fragments.main.recycler_view
+
+class MainViewHolderFactory {
+}
